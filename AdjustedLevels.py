1b09e0e91b4588828dee99af908aca45601e7b3c
SyrineSouid <syrinesouid8@gmail.com>
2024-04-23 11:09:05 +0100

    Modifications and adjustments

--- AdjustedLevels.py
+++ AdjustedLevels.py
@@ -88,8 +88,8 @@ class CylinderWidget(QWidget):
         font = QFont("Arial", 15)
         painter.setFont(font)
         painter.setPen(QPen(QColor(194,221,228), 8))
-        painter.drawText(level_x + 8, tank_y + tank_height + 15, "0%")  # 0% label
-        painter.drawText(level_x + 8, tank_y - 5, "100%")  # 100% label
+        painter.drawText(level_x + 5, tank_y + tank_height + 25, "0%")  # 0% label
+        painter.drawText(level_x + 5, tank_y - 20, "100%")  # 100% label
 
         # Draw tank name
         painter.drawText(tank_x + 80, tank_y + tank_height + 30, self.tank_name)
@@ -99,24 +99,24 @@ class CylinderWidget(QWidget):
             painter.setPen(QColor("#BA1301"))
             painter.setBrush(QColor("#BA1301"))
             if self.tank_level==0:
-                painter.drawText(tank_x, tank_y + tank_height-320, f"Tank Level={int(self.tank_level * 100)}% : EMPTY TANK")
+                painter.drawText(tank_x-20, tank_y + tank_height-322, f"Tank Level={int(self.tank_level * 100)}% : EMPTY TANK")
             else:
-                painter.drawText(tank_x, tank_y + tank_height-320, f"Tank Level={int(self.tank_level * 100)}% : CRITICAL")
+                painter.drawText(tank_x-20, tank_y + tank_height-322, f"Tank Level={int(self.tank_level * 100)}% : CRITICAL")
         elif 0.25 <= self.tank_level < 0.51:
             painter.setPen(QColor("#E4670B"))
             painter.setBrush(QColor("#E4670B"))
-            painter.drawText(tank_x, tank_y + tank_height-320, f"Tank Level={int(self.tank_level * 100)}% : MODERATE")
+            painter.drawText(tank_x-20, tank_y + tank_height-322, f"Tank Level={int(self.tank_level * 100)}% : MODERATE")
         elif 0.5 <= self.tank_level < 0.76:
             painter.setPen(QColor("#EBA104"))
             painter.setBrush(QColor("#EBA104"))
-            painter.drawText(tank_x, tank_y + tank_height-320, f"Tank Level={int(self.tank_level * 100)}% : GOOD")
+            painter.drawText(tank_x-20, tank_y + tank_height-322, f"Tank Level={int(self.tank_level * 100)}% : GOOD")
         elif 0.75 <= self.tank_level <= 1.0:
             painter.setPen(QColor("#94C816"))
             painter.setBrush(QColor("#94C816"))
             if self.tank_level==1:
-                painter.drawText(tank_x, tank_y + tank_height-320, f"Tank Level={int(self.tank_level * 100)}% : FULL TANK")
+                painter.drawText(tank_x-20, tank_y + tank_height-322, f"Tank Level={int(self.tank_level * 100)}% : FULL TANK")
             else:
-                painter.drawText(tank_x, tank_y + tank_height-320, f"Tank Level={int(self.tank_level * 100)}% : HIGH")
+                painter.drawText(tank_x-20, tank_y + tank_height-322, f"Tank Level={int(self.tank_level * 100)}% : HIGH")
         
         painter.drawText(level_x+20, level_indicator_y - level_indicator_height // 2, f"{int(self.tank_level * 100)}%")
 
@@ -124,22 +124,7 @@ class CylinderWidget(QWidget):
         tank_fill_height = tank_height * self.tank_level  # Calculate the height of the filled area
         tank_fill_y = tank_y + tank_height - tank_fill_height  # Calculate the y-coordinate of the filled area
 
-        """if self.tank_level <= 0.3:  # If the tank level is 30% or less
-            # Draw only the bottom ellipse for the filled area
-            painter.drawEllipse(QRect(int(tank_x), int(tank_y + tank_height - tank_width // 2), int(tank_width), int(tank_width // 2)))"""
-        
-        """if self.tank_level <= 0.29 and self.tank_level>0.19:  # If the tank level is 30% or less
-            # Calculate the height and width of the bottom ellipse
-            bottom_ellipse_height = tank_height * self.tank_level * 1.2  # Adjusting the factor (1.5) to fit visually
-            # Draw the bottom ellipse for the filled area
-            painter.drawEllipse(QRect(int(tank_x), int(tank_y + tank_height - bottom_ellipse_height), int(tank_width), int(bottom_ellipse_height)))
-        
-        elif self.tank_level<=0.19:
-            bottom_ellipse_height = tank_height * self.tank_level * 1.2  # Adjusting the factor (1.5) to fit visually
-            bottom_ellipse_width = tank_width*3/4 # Adjusting the factor (1.5) to fit visually
-            # Draw the bottom ellipse for the filled area
-            painter.drawEllipse(QRect(int(tank_x + (tank_width - bottom_ellipse_width) / 2), int(tank_y + tank_height - bottom_ellipse_height), int(bottom_ellipse_width), int(bottom_ellipse_height)))"""
-        
+
         if self.tank_level <= 0.29 and self.tank_level > 0.19:  # If the tank level is between 20% and 30%
             # Calculate the height and width of the bottom ellipse
             bottom_ellipse_height = tank_height * self.tank_level * 1.2  # Adjusting the factor (1.5) to fit visually
@@ -170,9 +155,6 @@ class CylinderWidget(QWidget):
 
             # Draw the curved sides of the filled area
             painter.drawRect(int(tank_x), int(tank_fill_y + tank_width // 4), int(tank_width), int(tank_fill_height - tank_width // 2))
-
-
-
 
         
 if __name__ == "__main__":
