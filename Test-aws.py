4307fd42d14f43ba1d700491baa1110f51e03d8f
khawla-bk <benkadidakhawla@gmail.com>
2024-11-11 12:45:56 +0100

    added certs

--- Test-aws.py
+++ Test-aws.py
@@ -9,7 +9,7 @@ def test(self,params,packet):
 myMQTTClient=AWSIoTMQTTClient("Raspberry_pi")
 myMQTTClient.configureEndpoint("a2w3ke0es02zef-ats.iot.eu-west-3.amazonaws.com", 8883)
 
-myMQTTClient.configureCredentials("/home/gateway/Downloads/Cert/root-CA.crt", "/home/gateway/Downloads/Cert/Raspberry_pi.cert.pem")  
+myMQTTClient.configureCredentials("/home/gateway/Desktop/PFE-2024/Cert/root-CA.crt",  "/home/gateway/Desktop/PFE-2024/Cert/Raspberry_pi.cert.pem", "/home/gateway/Desktop/PFE-2024/Cert/Raspberry_pi.private.key")  
 myMQTTClient.configureOfflinePublishQueueing(-1) # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2) # Draining: 2 Hz
 myMQTTClient.configureConnectDisconnectTimeout(10) # 10 sec
