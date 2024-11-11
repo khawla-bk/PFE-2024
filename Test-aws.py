a3bb5aad29b6268b21dfa250bdec2a6ab96476d8
khawla-bk <benkadidakhawla@gmail.com>
2024-11-11 12:59:52 +0100

    updated test aws

--- Test-aws.py
+++ Test-aws.py
@@ -1,29 +1,38 @@
 import time
-from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-import logging
-logger = logging.getLogger("AWSIoTPythonSDK.core")
-logger.setLevel(logging.DEBUG)
-streamHandler = logging.StreamHandler()
-logger.addHandler(streamHandler)
+from awsiot import mqtt_connection_builder
+from awscrt import io, mqtt, auth, http
 
+# Define endpoint and cert paths
+endpoint = "a2w3ke0es02zef-ats.iot.eu-west-3.amazonaws.com"
+root_ca_path = "/home/gateway/Desktop/PFE-2024/Cert/root-CA.crt"
+cert_path = "/home/gateway/Desktop/PFE-2024/Cert/Raspberry_pi.cert.pem"
+key_path = "/home/gateway/Desktop/PFE-2024/Cert/Raspberry_pi.private.key"
 
-def test(self,params,packet):
-        print("Recieved message from AWS Iot Core")
-        print('Topic:'+packet.topic)
-        print("Payload: ",(packet.payload))
+# Set up MQTT connection using the AWS IoT Device SDK v2
+mqtt_connection = mqtt_connection_builder.mtls_from_path(
+        endpoint=endpoint,
+        cert_filepath=cert_path,
+        pri_key_filepath=key_path,
+        ca_filepath=root_ca_path,
+        client_id="Raspberry_pi",
+        clean_session=False,
+        keep_alive_secs=6,
+)
 
-myMQTTClient=AWSIoTMQTTClient("Raspberry_pi")
-myMQTTClient.configureEndpoint("a2w3ke0es02zef-ats.iot.eu-west-3.amazonaws.com", 8883)
+print("Connecting to AWS IoT...")
+connect_future = mqtt_connection.connect()
+connect_future.result()
+print("Connected!")
 
-myMQTTClient.configureCredentials("/home/gateway/Desktop/PFE-2024/Cert/root-CA.crt",  "/home/gateway/Desktop/PFE-2024/Cert/Raspberry_pi.cert.pem", "/home/gateway/Desktop/PFE-2024/Cert/Raspberry_pi.private.key")  
-myMQTTClient.configureOfflinePublishQueueing(-1) # Infinite offline Publish queueing
-myMQTTClient.configureDrainingFrequency(2) # Draining: 2 Hz
-myMQTTClient.configureConnectDisconnectTimeout(10) # 10 sec
-myMQTTClient.configureMQTTOperationTimeout(50) # 50 sec
-print ('Initiating connection...')
-myMQTTClient.connect()
-myMQTTClient.subscribe("Raspberry-test",1,test)
-while True:
-        time.sleep(5)
-        print("Publishing to AWS IoT Core")
-        myMQTTClient.publish(topic="Raspberry-test",QoS=1,payload="{'Message':'Hello From Rpi to AWS'}")
+# Publish a message
+mqtt_connection.publish(
+        topic="Raspberry-test",
+        payload="{'Message':'Hello From Rpi to AWS'}",
+        qos=mqtt.QoS.AT_LEAST_ONCE,
+)
+print("Published message to AWS IoT Core")
+
+# Disconnect
+disconnect_future = mqtt_connection.disconnect()
+disconnect_future.result()
+print("Disconnected!")
